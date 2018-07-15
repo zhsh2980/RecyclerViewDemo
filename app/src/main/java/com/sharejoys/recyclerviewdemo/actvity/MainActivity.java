0738fbbbd2b688093e4ad1cb131c6455cf96b36a
shanz01 <shanz01@jumei.com>
2018-07-15 23:51:27 +0800

    first init

--- app/src/main/java/com/sharejoys/recyclerviewdemo/actvity/MainActivity.java
+++ app/src/main/java/com/sharejoys/recyclerviewdemo/actvity/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //first init
+        //第二次提交
 
         list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
